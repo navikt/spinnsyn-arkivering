2f663d36321ae5597d9e6a5429cc78a96d8f5b93
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-01-24 15:12:44 +0100

    Les topic med uarkiverte på nytt

---
+++ src/main/kotlin/no/nav/helse/flex/kafka/ArkiveringListener.kt
@@ -0,0 +1,39 @@
+package no.nav.helse.flex.kafka
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.logger
+import no.nav.helse.flex.objectMapper
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+
+@Component
+class ArkiveringListener() {
+
+    private val log = logger()
+
+    @KafkaListener(
+        topics = [FLEX_VEDTAK_ARKIVERING_TOPIC],
+        containerFactory = "aivenKafkaListenerContainerFactory",
+        properties = ["auto.offset.reset = earliest"],
+        groupId = "spinnsyn-arkivering-ferdigstilling"
+    )
+    fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
+        val vedtak = cr.value().tilUarkivertVedtak()
+        log.info(
+            "Lest vedtak med id: ${vedtak.id} fra topic: $FLEX_VEDTAK_ARKIVERING_TOPIC, " +
+                "partisjon: ${cr.partition()} og offset: ${cr.offset()}."
+        )
+        acknowledgment.acknowledge()
+    }
+
+    fun String.tilUarkivertVedtak(): VedtakArkiveringDTO = objectMapper.readValue(this)
+}
+
+const val FLEX_VEDTAK_ARKIVERING_TOPIC = "flex.vedtak-arkivering"
+
+data class VedtakArkiveringDTO(
+    val fnr: String,
+    val id: String,
+)
