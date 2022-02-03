ea72ca4d64f6cad3460528c4d4592591c6d3c11a
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-02-03 09:50:39 +0100

    Test etter patching av Joark

--- src/main/kotlin/no/nav/helse/flex/kafka/ArkiveringListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/ArkiveringListener.kt
@@ -20,7 +20,7 @@ class ArkiveringListener(
         topics = [FLEX_VEDTAK_ARKIVERING_TOPIC],
         containerFactory = "aivenKafkaListenerContainerFactory",
         properties = ["auto.offset.reset = earliest"],
-        groupId = "spinnsyn-arkivering-ferdigstilling-test"
+        groupId = "spinnsyn-arkivering-ferdigstilling-test-1"
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         val arkivertVedtakDto = cr.value().tilArkivertVedtakDto()
