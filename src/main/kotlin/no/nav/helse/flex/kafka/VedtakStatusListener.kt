bd396a249b78865208adb2e5634f9c582c28d95e
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-08-19 08:44:09 +0200

    Skip feil i PDL

--- src/main/kotlin/no/nav/helse/flex/kafka/VedtakStatusListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/VedtakStatusListener.kt
@@ -2,6 +2,8 @@ package no.nav.helse.flex.kafka
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.arkivering.Arkivaren
+import no.nav.helse.flex.config.EnvironmentToggles
+import no.nav.helse.flex.logger
 import no.nav.helse.flex.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
@@ -11,7 +13,10 @@ import org.springframework.stereotype.Component
 @Component
 class VedtakStatusListener(
     val arkivaren: Arkivaren,
+    private val environment: EnvironmentToggles,
 ) {
+    val log = logger()
+
     @KafkaListener(
         topics = [FLEX_VEDTAK_STATUS_TOPIC],
         containerFactory = "aivenKafkaListenerContainerFactory",
@@ -21,8 +26,17 @@ class VedtakStatusListener(
         acknowledgment: Acknowledgment,
     ) {
         val vedtak = cr.value().tilVedtak()
-        arkivaren.arkiverVedtak(vedtak)
-        acknowledgment.acknowledge()
+        try {
+            arkivaren.arkiverVedtak(vedtak)
+            acknowledgment.acknowledge()
+        } catch (e: Exception) {
+            if (!environment.isProduction()) {
+                if (e.message?.contains("Fant ikke aktørid for person i pdl") == true) {
+                    log.info("Fant ikke aktørid for person i pdl, ignorerer melding siden vi er i dev " + vedtak.id)
+                    acknowledgment.acknowledge()
+                }
+            }
+        }
     }
 
     fun String.tilVedtak(): VedtakStatusDto = objectMapper.readValue(this)
