0dfa7e4c6e70ba688adf1bef1826e0adfc9c9fa3
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-01-14 15:05:54 +0100

    Hopper over vedtak etter fiks
    Co-authored-by: Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>

--- src/main/kotlin/no/nav/helse/flex/kafka/ArkiveringFixVedtakStatusListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/ArkiveringFixVedtakStatusListener.kt
@@ -12,6 +12,7 @@ import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneOffset
 
@@ -26,14 +27,21 @@ class ArkiveringFixVedtakStatusListener(
     @KafkaListener(
         topics = [FLEX_VEDTAK_STATUS_TOPIC],
         containerFactory = "aivenKafkaListenerContainerFactory",
-        groupId = "flex-arkivering-fix-vedtak-status",
+        groupId = "flex-arkivering-fix-vedtak-status-2",
         properties = ["auto.offset.reset = earliest"],
     )
     fun listen(
         cr: ConsumerRecord<String, String>,
         acknowledgment: Acknowledgment,
     ) {
-        log.info("Offset: ${cr.offset()} Partition: ${cr.partition()} Timestamp: ${cr.timestamp()}")
+        val tidspunkt = Instant.ofEpochMilli(cr.timestamp()).atOffset(ZoneOffset.UTC)
+        if (tidspunkt.isAfter(LocalDate.of(2026, 1, 14).atStartOfDay().atOffset(ZoneOffset.UTC))) {
+            log.info("Hopper over vedtak med timestamp $tidspunkt siden det er etter fiks")
+            acknowledgment.acknowledge()
+            return
+        }
+
+        log.info("Offset: ${cr.offset()} Partition: ${cr.partition()} Timestamp: $tidspunkt")
 
         val vedtak = cr.value().tilVedtak()
         log.info("Mottok vedtak med id: ${vedtak.id} og status: ${vedtak.vedtakStatus}")
